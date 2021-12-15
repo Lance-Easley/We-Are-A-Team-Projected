ff90a23de3a88f9bb5eef6a0643f5ba1d36b95a8
Peyton Carver <pecarver@corelogic.com>
2021-12-15 14:56:33 -0600

    added second table to Groups page

---
+++ Projected/wwwroot/js/Projects.js
@@ -0,0 +1,33 @@
+﻿function ProjectViewModel(id) {
+    let self = this;
+    
+
+    self.selectedId = ko.observable();
+    self.profile = ko.observable();
+    self.projectName = ko.observable();
+    self.permissionGroups = ko.observableArray();
+
+
+    self.getGroups = function () {
+
+        $.ajax({
+            url: "../api/profiles/" + id,
+            type: 'GET',
+            success: function (data) {
+                self.profile(data);
+                console.log(self.profile());
+                self.projectName(self.profile().projectName);
+                self.permissionGroups(self.profile().permissionGroups);
+                console.log(self.permissionGroups());
+
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                toastr.error(jqXHR.responseText);
+            },
+            complete: function () {
+            }
+        });
+    }
+
+
+}
